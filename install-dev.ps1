d6174dca5f68341fe5880ba6f5ae91e0db63c156
Jeffrey Hart <jeffrey.m.hart@avanade.com>
2019-06-24 12:18:50 -0500

    Initial

---
+++ install-dev.ps1
@@ -0,0 +1,44 @@
+#git
+choco install git -y
+
+#vim
+choco install vim -y
+
+#vscode 
+choco install vscode -y
+
+#visual studio
+choco install visualstudio2019professional -y
+
+#dotnet core sdk
+choco install dotnetcore-sdk -y
+
+#python
+choco install python -y
+
+#nodejs
+choco install nodejs -y
+
+#sqllite
+choco install sqllite -y
+
+#mysql
+choco install mysql -y
+
+#linqpad
+choco install linqpad -y
+
+#ssms
+choco install sql-server-management-studio -y
+
+#mongodb
+choco install mongodb -y
+
+#postman
+choco install postman -y
+
+#autohotkey
+choco install autohotkey -y
+
+#filezilla
+choco install filezilla -y
---
+++ install-personal.ps1
@@ -0,0 +1,26 @@
+#1password -- not working
+choco install 1password -y
+
+#spotify
+choco install spotify -y
+
+#ccleaner
+choco install ccleaner -y
+
+#malwarebytes
+choco install malwarebytes -y
+
+#windirstat
+choco install windirstat -y
+
+#steam
+choco install steam -y
+
+#slack
+choco install slack -y
+
+#discord
+choco install discord -y
+
+#kindle
+choco install kindle -y
